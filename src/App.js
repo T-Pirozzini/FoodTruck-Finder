8a67aca5ba6a481740fc665b305706c539f50ad6
BBrandford11 <brandfordbryce@gmail.com>
2022-04-14 19:14:36 -0600

    adds map to main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import MyComponent from './Map';
+
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{width: "100%", height: "100%"}}>
       Foodtruck
+      
+      <MyComponent />
     </div>
   );
 }
