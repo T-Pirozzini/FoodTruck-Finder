d319a0cf9bb4760317bb116d7a02f50e2c094110
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-04-15 20:08:49 -0700

    implement use fetch

--- src/components/TruckCard.js
+++ src/components/TruckCard.js
@@ -1,24 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import { useFetch } from '../hooks/useFetch'
+import client from '../../data/elephantSQL'
 
 // styles
 import './TruckCard.css'
 
-export default function TruckCard() {
-  const [pins, setPins] = useState([])
-  const [url, setUrl] = useState(['http://localhost:3000/foodTrucks']) 
-
-  useEffect(() => {
-    fetch(url)
-      .then(res => res.json())
-      .then(json => setPins(json))
-  }, [url])
+export default function TruckCard() {  
+  // const [url, setUrl] = useState(['http://localhost:3000/foodTrucks'])
+  const [url, setUrl] = useState([client])
+  const { data: pins } = useFetch(url)  
 
   console.log(pins)
 
   return (
     <div className="main-container">
       <div className="cards-container">        
-        {pins.map(pin => (
+        {pins && pins.map(pin => (
           <div key={pin.id} className="truck-card">
             <h3>{pin.truck_name}</h3>
             <p>{pin.info}</p>
