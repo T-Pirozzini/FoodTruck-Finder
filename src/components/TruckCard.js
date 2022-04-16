3cc5a248b43dfd8475e429764de8694a5633581a
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-04-15 17:13:08 -0700

    implement filter buttons

--- src/components/TruckCard.js
+++ src/components/TruckCard.js
@@ -4,28 +4,41 @@ import React, { useState, useEffect } from 'react'
 import './TruckCard.css'
 
 export default function TruckCard() {
-  const [pins, setPins] = useState([])  
+  const [pins, setPins] = useState([])
+  const [url, setUrl] = useState(['http://localhost:3000/foodTrucks']) 
 
   useEffect(() => {
-    fetch('http://localhost:3000/foodTrucks')
+    fetch(url)
       .then(res => res.json())
       .then(json => setPins(json))
-  }, [])
+  }, [url])
 
   console.log(pins)
 
   return (
-    <div className="cards-container">        
-      {pins.map(pin => (
-        <div key={pin.id} className="truck-card">
-          <h3>{pin.truck_name}</h3>
-          <p>{pin.info}</p>
-          <p>{pin.rating}</p>
-          <p>{pin.location_lng}, {pin.location_lat}</p>            
-        </div>
-      ))}    
-  </div> 
-
+    <div className="main-container">
+      <div className="cards-container">        
+        {pins.map(pin => (
+          <div key={pin.id} className="truck-card">
+            <h3>{pin.truck_name}</h3>
+            <p>{pin.info}</p>
+            <p>{pin.rating}</p>
+            <p>{pin.location_lng}, {pin.location_lat}</p>
+          </div>        
+        ))}
+      </div>    
+      <div className="filters">
+        <button onClick={() => setUrl("http://localhost:3000/foodTrucks")}>
+          All
+        </button> 
+        <button onClick={() => setUrl("http://localhost:3000/foodTrucks?category=potato")}>
+          Spuds
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/foodTrucks?category=coffee")}>
+          Coffee
+        </button>        
+      </div>
+    </div>      
   )
 }
 
