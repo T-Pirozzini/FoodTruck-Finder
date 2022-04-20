ef58489f679e2be8c65c3d0e7e7e841777787a9b
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-04-20 12:51:46 -0700

    implement schedule component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
 import { Link } from 'react-router-dom';
-
+import Schedule from './Schedule';
 
 // styles
 import './Nav.css'
 
 export default function Nav() {
   return (
-    <div className="nav-container">
+    
+    <div className="nav-container">      
       <div className="nav-title">
         <h2><Link to="/">FoodTruck Finder</Link></h2>
       </div>
+      <Schedule />
       <div className="nav-items">      
         <div className="link">Favourites</div>
 
