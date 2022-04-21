7f19d9e094af88d515243c421e6d2864f40bcdf5
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-04-21 10:37:49 -0700

    add register truck map and complete change over to leaflet

--- src/pages/signup/MyTruckLocation.js
+++ src/pages/signup/MyTruckLocation.js
@@ -1,79 +1,53 @@
 import React, { useState, useEffect } from 'react'
 
+// components
 import SignUpATruck from './SignUpATruck';
 
+// Leaflet API
 import { MapContainer, TileLayer, useMap, Marker, Tooltip, Popup, useMapEvents } from 'react-leaflet'
 import L from 'leaflet';
 
+// styles
 import styles from "./Signup.module.css"
-
 import 'leaflet/dist/leaflet.css';
 
+
 const truckIcon = new L.Icon({
-  iconUrl: "https://cdn-icons.flaticon.com/png/128/499/premium/499552.png?token=exp=1650510932~hmac=9bb9406da449bd57dffea26b0071cb39",
-  // iconRetinaUrl: require("../assets/pointerIcon.svg"),  
-  popupAnchor: [1, -1],
-  iconSize: [55, 55],
-  // shadowUrl: "https://cdn-icons-png.flaticon.com/128/7314/7314532.png",
-  // shadowSize: [68, 95],
-  // shadowAnchor: [20, 92]
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",  
+  iconSize: [30, 30],  
 });
 
-// function LocationMarker() {
-//   const [position, setPosition] = useState(null)
-//   const map = useMapEvents({
-//     click: (e) => {
-//       const { lat, lng } = e.latlng;
-//       L.marker([lat, lng], { truckIcon }).addTo(map);
-//     }
-//   });
-//   return null;
-// }
-
-
-
-// const location = () => {
-  
-//   console.log("target")
-// }
-
 export default function MyTruckLocationMap() {
-
-  // const [map, setMap] = React.useState(null)
-  // const [cords,setCords] = React.useState({ 
-  //   lat: 53.5461,
-  //   lng: -113.4938}) 
- 
+  const [cords, setCords] = useState([53.5456, -113.4903])   
+  const LocationMarker = () => {  
+    const map = useMapEvents({
+      click: (e) => {      
+        const { lat, lng } = e.latlng;        
+        setCords([lat,lng])        
+      }
+    });
+    return null;
+  }
 
   return (
     <>
-
-    <>
-    
+    <>    
+    <SignUpATruck cords={cords} />
       <MapContainer 
         center={[53.5456, -113.4903]} 
         zoom={13} 
-        scrollWheelZoom={true} 
-        // onClick={((e) => {console.log(e.target)})}
-        eventHandlers={{ click: console.log("click")}}
-       
+        scrollWheelZoom={true}          
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"         
-          />
-        {/* <LocationMarker /> */}
+          />        
         <Marker
-        position={[53.5456, -113.4903]}
-        eventHandlers={{
-          click: (e) => {
-            {console.log(e)}
-          },
-        }}
-        
-        >
-          
-        </Marker>        
+        position={cords}
+        icon={ truckIcon }       
+        >          
+        </Marker> 
+        <LocationMarker />       
       </MapContainer>
     </>
     </>
--- src/pages/signup/SignUpATruck.js
+++ src/pages/signup/SignUpATruck.js
@@ -3,32 +3,30 @@ import { useState } from 'react'
 // import MyTruckLocationMap from "./MyTruckLocation"
 import {Select, InputLabel, FormControl, MenuItem} from '@mui/material';
 
-
-
 export default function SignUpATruck(props) {
   const [name, setName] = useState('')
   const [info, setInfo] = useState('')
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault()
-  //   console.log("name", name)
-  //   const lat = props.cords.lat
-  //   const lng = props.cords.lng
-  //   const rating = 5
-  //   const attributes = {name, info, rating , lat, lng}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("name", name)
+    const lat = props.cords[0]
+    const lng = props.cords[1]
+    const rating = 5
+    const attributes = {name, info, rating , lat, lng}
 
-  //   fetch("http://localhost:3002/signup",{
-  //     method:"POST",
-  //     headers: {"Content-Type": "application/json" },
-  //     body: JSON.stringify(attributes)
+    fetch("http://localhost:3002/signup",{
+      method:"POST",
+      headers: {"Content-Type": "application/json" },
+      body: JSON.stringify(attributes)
     
-  //   }).then(() => {
-  //     console.log("Truck Registered");
-  //   })
-  // }
+    }).then(() => {
+      console.log("Truck Registered");
+    })
+  }
 
   return (    
-    <form   className="registration-form">
+    <form  onSubmit={handleSubmit} className="registration-form">
       <h2>Register Your FoodTruck</h2>
       <label>
         <span>Name:</span>
@@ -39,6 +37,7 @@ export default function SignUpATruck(props) {
         />        
       </label>      
       
+      {/* Drop Down Info */}
       <FormControl fullWidth >
         <InputLabel>Info:</InputLabel>
         <Select          
@@ -51,22 +50,16 @@ export default function SignUpATruck(props) {
           <MenuItem value={"bbq"}>BBQ</MenuItem>
         </Select>
       </FormControl>
-
-      <label>
-       
-      </label>
+      
       <label>
         <span>Latitude:</span>
-        {/* {props.cords.lat} */}
-
+        {props.cords[0]}
       </label>
       <label>
         <span>Longitude:</span>
-        {/* {props.cords.lng} */}
+        {props.cords[1]}
       </label>
-      <button className="submit-btn" type="submit">Submit</button>
-
-      
+      <button className="submit-btn" type="submit">Submit</button>      
     </form>        
   )
 }
