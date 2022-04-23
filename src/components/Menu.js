747cbd17e37b032601c21f219772b1338d91e231
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-04-22 19:28:59 -0700

    add quantity/settotals

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Menu = ({ items }) => {
+ const [quantity, setQuantity] = useState(0);
+ const [total, setTotal] = useState(0);
+
+ const updateQuantity = (e, price) => {
+  // console.log(e.target.value)   
+  // setQuantity(e.target.value)
+  console.log(price)
+  setTotal(price*e.target.value)
+  
+}
+
+// helper function to updat the total and then call this function inside of update quantity
+
   return (
     <div className="section-center">
       {items.map((menuItem) => {
         const { id, title, img, price, desc } = menuItem
-        return (
+        return (          
           <article key={id} className="menu-item">
             <img src={img} alt={title} className="photo" />
             <div className="item-info">
@@ -14,12 +28,26 @@ const Menu = ({ items }) => {
                 <h4 className="price">${price}</h4>
               </header>
               <p className="item-text">{desc}</p>
+              <label>Quantity:</label>           
+              <input className="quantity" name="quantity" type="number" min="0" max="10" onChange={(event) => updateQuantity(event, price)}></input>
             </div>
-          </article>
+          </article>         
         )
+    
       })}
-    </div>
+        <article className="order-total">
+          {total}
+        </article>       
+          
+    </div>     
   )
+  
 }
+ 
 
 export default Menu
+
+
+
+
+{/* <button onClick={(e) => this.deleteRow(id, e)}>Delete Row</button> */}
