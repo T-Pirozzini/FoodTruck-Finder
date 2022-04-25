f948cd12b363350762a7d0a87006beba749f1a69
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-04-25 14:30:58 -0700

    add truck styling and add menu images

--- src/pages/signup/SignUpATruck.js
+++ src/pages/signup/SignUpATruck.js
@@ -45,83 +45,88 @@ export default function SignUpATruck(props) {
   return (       
     <form  onSubmit={handleSubmit} className="registration-form">
       <h2 className='registration-components'>Food Truck Registration</h2>
-      <label className='registration-components name-of-truck'>
+      <label className='name-of-truck'>
         <span>Name Your Truck:</span>
         
         <input 
         // type="email"
+        id="truck-input"
         onChange={(e) => setName(e.target.value)}
         value={name} 
         />        
       </label>      
       
       {/* Drop Down Info */}
-      <FormControl fullWidth  className='registration-components'>
-        <div className="input-label">
-          <InputLabel>Your truck specializes in </InputLabel>
-        </div>
-        <div className="input-options">
-          <Select          
-            id="info-items"
-            value={info}          
-            onChange={(e) => setInfo(e.target.value)}                 
-          >
-            <MenuItem value={"coffee"}>Coffee</MenuItem>
-            <MenuItem value={"potato"}>Potatos</MenuItem>
-            <MenuItem value={"bbq"}>BBQ</MenuItem>
-            <MenuItem value={"nachos"}>Nachos</MenuItem>
-            <MenuItem value={"dessert"}>Dessert</MenuItem>
-            <MenuItem value={"burgers"}>Burgers</MenuItem>
-          </Select>
-        </div>
-      </FormControl>      
-            
-      <table>
-        <thead>
-          <tr>
-            <th>Day</th>
-            <th>Latitude</th>
-            <th>Longitude</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Monday</td>
-            <td>{props.dayLocation[1].lat}</td>
-            <td>{props.dayLocation[1].lng}</td>
-          </tr>
-          <tr>
-            <td>Tuesday</td>
-            <td>{props.dayLocation[2].lat}</td>
-            <td>{props.dayLocation[2].lng}</td>
-          </tr>
-          <tr>
-            <td>Wednesday</td>
-            <td>{props.dayLocation[3].lat}</td>
-            <td>{props.dayLocation[3].lng}</td>
-          </tr>
-          <tr>
-            <td>Thursday</td>
-            <td>{props.dayLocation[4].lat}</td>
-            <td>{props.dayLocation[4].lng}</td>
-          </tr>
-          <tr>
-            <td>Friday</td>
-            <td>{props.dayLocation[5].lat}</td>
-            <td>{props.dayLocation[5].lng}</td>
-          </tr>
-          <tr>
-            <td>Saturday</td>
-            <td>{props.dayLocation[6].lat}</td>
-            <td>{props.dayLocation[6].lng}</td>
-          </tr>
-          <tr>
-            <td>Sunday</td>
-            <td>{props.dayLocation[7].lat}</td>
-            <td>{props.dayLocation[7].lng}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="dropdown-container">
+        <FormControl fullWidth>
+          <div id="input-label">
+            <InputLabel><div className="text">Your truck specializes in</div></InputLabel>
+          </div>
+          <div id="input-options">
+            <Select          
+              id="info-items"
+              value={info}          
+              onChange={(e) => setInfo(e.target.value)}                 
+            >
+              <MenuItem value={"coffee"}>Coffee</MenuItem>
+              <MenuItem value={"potato"}>Potatos</MenuItem>
+              <MenuItem value={"bbq"}>BBQ</MenuItem>
+              <MenuItem value={"nachos"}>Nachos</MenuItem>
+              <MenuItem value={"dessert"}>Dessert</MenuItem>
+              <MenuItem value={"burgers"}>Burgers</MenuItem>
+            </Select>
+          </div>
+        </FormControl>
+      </div>
+
+      <div className="table-container">     
+        <table>
+          <thead>
+            <tr>
+              <th>Day</th>
+              <th>Latitude</th>
+              <th>Longitude</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Monday</td>
+              <td>{props.dayLocation[1].lat}</td>
+              <td>{props.dayLocation[1].lng}</td>
+            </tr>
+            <tr>
+              <td>Tuesday</td>
+              <td>{props.dayLocation[2].lat}</td>
+              <td>{props.dayLocation[2].lng}</td>
+            </tr>
+            <tr>
+              <td>Wednesday</td>
+              <td>{props.dayLocation[3].lat}</td>
+              <td>{props.dayLocation[3].lng}</td>
+            </tr>
+            <tr>
+              <td>Thursday</td>
+              <td>{props.dayLocation[4].lat}</td>
+              <td>{props.dayLocation[4].lng}</td>
+            </tr>
+            <tr>
+              <td>Friday</td>
+              <td>{props.dayLocation[5].lat}</td>
+              <td>{props.dayLocation[5].lng}</td>
+            </tr>
+            <tr>
+              <td>Saturday</td>
+              <td>{props.dayLocation[6].lat}</td>
+              <td>{props.dayLocation[6].lng}</td>
+            </tr>
+            <tr>
+              <td>Sunday</td>
+              <td>{props.dayLocation[7].lat}</td>
+              <td>{props.dayLocation[7].lng}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div> 
       
 
       {/* Submit Button */}
